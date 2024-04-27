c02b9690922871df5f2f3e686733330b39f0ab23
LeoCarulli <leo.carulli@yahoo.com.ar>
2024-04-27 16:01:15 -0300

    ok2

--- src/workflow-01/501_workflow_guantesblancos_gbdt_testcompleto1.r
+++ src/workflow-01/501_workflow_guantesblancos_gbdt_testcompleto1.r
@@ -287,7 +287,7 @@ HT_tuning_guantesblancos <- function( pmyexp, pinputexps, pserver="local")
     bagging_fraction = c( 0.1, 0.9), # 0.0 < bagging_fraction <= 1.0
     feature_fraction = c( 0.1, 0.9),
     pos_bagging_fraction = c( 0.1, 0.9), # 0.0 < pos_bagging_fraction <= 1.0
-    neg_bagging_fraction = c( 0.1, 0.9), # 0.0 < neg_bagging_fraction <= 1.0
+    neg_bagging_fraction = c( 0.1, 0.9) # 0.0 < neg_bagging_fraction <= 1.0
   )
 
   # una Beyesian de Guantes Blancos, solo hace 15 iteraciones
