04b552610c20d18ed195833d0e2a330b50d871c3
LeoCarulli <leo.carulli@yahoo.com.ar>
2024-04-27 13:51:01 -0300

    test3

---
+++ src/workflow-01/subida_archivos_kaggle.py
@@ -0,0 +1,19 @@
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+# Configurar la API de Kaggle
+api = KaggleApi()
+api.authenticate()
+
+# Ruta de la carpeta con los archivos a subir
+carpeta_archivos = r'C:\Users\RYZEN\Desktop\Maestria_Austral\7-LabDeImplementación\ArchivosKaggle'
+
+# Obtener la lista de archivos en la carpeta
+archivos = os.listdir(carpeta_archivos)
+
+# Iterar sobre cada archivo y subirlo a la competencia
+for archivo in archivos:
+    ruta_completa = os.path.join(carpeta_archivos, archivo)
+    # Subir archivo a la competencia
+    api.competition_submit(ruta_completa, "GBDT Test 2", competition="labo-i-2024-virtual")
+    print(f"Archivo '{archivo}' subido con éxito.")
