01de63fdc08365b3c9d741f89a41603720bf8685
LeoCarulli <leo.carulli@yahoo.com.ar>
2024-05-05 18:12:54 -0300

    dart2

--- src/workflow-01/subida_archivos_kaggle.py
+++ src/workflow-01/subida_archivos_kaggle.py
@@ -15,5 +15,5 @@ archivos = os.listdir(carpeta_archivos)
 for archivo in archivos:
     ruta_completa = os.path.join(carpeta_archivos, archivo)
     # Subir archivo a la competencia
-    api.competition_submit(ruta_completa, "40 árboles RF test", competition="labo-i-2024-virtual")
+    api.competition_submit(ruta_completa, "Completo sin 3meses faltantes", competition="labo-i-2024-virtual")
     print(f"Archivo '{archivo}' subido con éxito.")
